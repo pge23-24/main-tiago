e61214bef3fdda96f3cc3fcae900c58474193863
ConstantRoux <constant.roux31@gmail.com>
2023-11-27 13:49:50 +0100

    Add auto connection to the network in the bash file

--- connect_to_robot.bash
+++ connect_to_robot.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Connexion au routeur du wi-fi
+SSID=PMB2-46c
+PWD=P@L-R0b0t1cs
+nmcli device wifi connect "$SSID" password "$PWD"
+
 # Chemin vers le setup.bash de ROS
 echo "Sourcing ROS Noetic"
 source /opt/ros/noetic/setup.bash
@@ -13,6 +18,6 @@ echo "Settuping ROS_MASTER_URI"
 export ROS_MASTER_URI=http://10.68.0.1:11311
 
 # Récupération de l'adresse IP du PC sur le réseau
-echo "Settuping ROS_IP"
+echo "Settuping ROS_IP	"
 export ROS_IP=$(hostname -I | cut -d' ' -f1)
 
