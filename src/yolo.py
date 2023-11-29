5d9db328dba62400667123948b0992fccccbe054
PGE-2023 <pierre.pincon@univ-tlse3.fr>
2023-11-29 09:40:47 +0100

    Update yolo.py
    Addition of comments and globalization of the model name

--- src/yolo.py
+++ src/yolo.py
@@ -1,12 +1,25 @@
+# Import the YOLO class from the ultralytics library
 from ultralytics import YOLO
 
+# Global variable for the name of the model
+# "yolov8s-pose.pt" is a pre-trained model for pose estimation
+MODEL = "yolov8s-pose.pt"
+
+# Define a class named 'yolo'
 class yolo:
     def __init__(self):
-        self.model = YOLO("yolov8s-pose.pt")
+        # Initialize a YOLO model with the specified weights file
+        self.model = YOLO(MODEL)
+
     def compute(self, image):
-        # Check if the image was successfully loaded
+        # This function processes an image using the YOLO model
+
+        # Check if the image input is valid
         if image is not None:
-            # Run YOLOv8 tracking on the image
+            # Run YOLOv8 pose estimation on the provided image
+            # 'persist=True' enables persistent tracking across frames
+            # 'tracker="bytetrack.yaml"' specifies the tracking configuration to use
             results = self.model.track(image, persist=True, tracker="bytetrack.yaml")
 
+            # Return the results of the model
             return results
