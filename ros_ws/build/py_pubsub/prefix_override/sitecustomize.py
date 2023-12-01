bf8358eda96e106a99b984cb4cd87be5a0e15e7c
PGE-2023 <pierre.pincon@univ-tlse3.fr>
2023-12-01 11:41:02 +0100

    Update structure for ros_ws

--- ros_ws/build/py_pubsub/prefix_override/sitecustomize.py
+++ ros_ws/build/py_pubsub/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/pge-2023/vis2-tiago/src/py_pubsub/install/py_pubsub'
+sys.prefix = sys.exec_prefix = '/home/pge-2023/vis2-tiago/ros_ws/install/py_pubsub'
