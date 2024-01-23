c71f25b0497051e9f265010501220c474a5bdcb4
Pierre Pinçon <pierre.pincon@univ-tlse3.fr>
2024-01-23 11:11:31 +0100

    Update 4to1image.py
    Formatting changed

--- prototyping/src/surround_yolo/4to1image.py
+++ prototyping/src/surround_yolo/4to1image.py
@@ -5,50 +5,65 @@ import os
 
 
 class CvFunction:
-        def calibrate(self, folderImages):
-            # termination criteria
-            criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-            # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-            objp = np.zeros((6 * 7, 3), np.float32)
-            objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
-            # Arrays to store object points and image points from all the images.
-            objpoints = []  # 3d point in real world space
-            imgpoints = []  # 2d points in image plane.
-            for fname in os.listdir(folderImages):
-                img = cv.imread(fname)
-                gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-                # Find the chess board corners
-                ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
-                # If found, add object points, image points (after refining them)
-                if ret is True:
-                    objpoints.append(objp)
-                    corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-                    imgpoints.append(corners2)
-                    # Draw and display the corners
-                    cv.drawChessboardCorners(img, (7, 6), corners2, ret)
-                    cv.imshow('img', img)
-                    cv.waitKey(500)
-            cv.destroyAllWindows()
-            K = None
-            D = None
-            ret, mtx, dist, rvecs, tvecs = cv.fisheye.calibrate(objpoints, imgpoints, np.shape(fname), K, D)
+    def calibrate(self, folderImages):
+        # termination criteria
+        criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+        objp = np.zeros((6 * 7, 3), np.float32)
+        objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+        # Arrays to store object points and image points from all the images.
+        objpoints = []  # 3d point in real world space
+        imgpoints = []  # 2d points in image plane.
+        for fname in os.listdir(folderImages):
+            img = cv.imread(fname)
+            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            # Find the chess board corners
+            ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
+            # If found, add object points, image points (after refining them)
+            if ret is True:
+                objpoints.append(objp)
+                corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+                imgpoints.append(corners2)
+                # Draw and display the corners
+                cv.drawChessboardCorners(img, (7, 6), corners2, ret)
+                cv.imshow("img", img)
+                cv.waitKey(500)
+        cv.destroyAllWindows()
+        K = None
+        D = None
+        ret, mtx, dist, rvecs, tvecs = cv.fisheye.calibrate(
+            objpoints, imgpoints, np.shape(fname), K, D
+        )
 
-        def undistort(self, image, ret, mtx, dist):
-            h, w = image.shape[:2]
-            newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
-            # undistort
-            dst = cv.undistort(image, mtx, dist, None, newcameramtx)
-            # crop the image
-            x, y, w, h = roi
-            dst = dst[y:y + h, x:x + w]
-            cv.imwrite('calibresult.png', dst)
+    def undistort(self, image, ret, mtx, dist):
+        h, w = image.shape[:2]
+        newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+        # undistort
+        dst = cv.undistort(image, mtx, dist, None, newcameramtx)
+        # crop the image
+        x, y, w, h = roi
+        dst = dst[y : y + h, x : x + w]
+        cv.imwrite("calibresult.png", dst)
+
+
 class Fuze:
     def concatener_images_horizontalement(self, listImgCorrected):
-        assert listImgCorrected[0].shape[0] == listImgCorrected[1].shape[0] == listImgCorrected[2].shape[0] == \
-               listImgCorrected[3].shape[0], "Les hauteurs des images doivent être les mêmes."
+        assert (
+            listImgCorrected[0].shape[0]
+            == listImgCorrected[1].shape[0]
+            == listImgCorrected[2].shape[0]
+            == listImgCorrected[3].shape[0]
+        ), "Les hauteurs des images doivent être les mêmes."
 
-        result = np.concatenate((listImgCorrected[0], listImgCorrected[2], listImgCorrected[1], listImgCorrected[3]),
-                                axis=1)
+        result = np.concatenate(
+            (
+                listImgCorrected[0],
+                listImgCorrected[2],
+                listImgCorrected[1],
+                listImgCorrected[3],
+            ),
+            axis=1,
+        )
 
         return result
 
