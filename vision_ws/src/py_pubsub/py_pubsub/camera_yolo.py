eb3d9d0644ca6e97f6d288539c23c98d87fbdad9
Pierre Pinçon <pierre.pincon@univ-tlse3.fr>
2024-01-24 16:23:11 +0100

    Adaptation of ROS scripts in order to implement with robotics group
    ALL THE WORK OF JANUARY 24TH 2024 WAS MADE WITH PETER PIRIOU--DEZY
    
    Co-Authored-By: PeterDezy <82024789+PeterDezy@users.noreply.github.com>
    Co-Authored-By: dosrt <93156263+dosrt@users.noreply.github.com>

--- vision_ws/src/py_pubsub/py_pubsub/camera_yolo.py
+++ vision_ws/src/py_pubsub/py_pubsub/camera_yolo.py
@@ -10,6 +10,7 @@ from ultralytics.utils import LOGGER  # LOGGER import
 from rclpy.utilities import remove_ros_args
 import sys
 import torch
+from py_pubsub.msg import ClassCoordinates
 
 
 class YOLOv8Detector:
@@ -24,6 +25,7 @@ class YOLOv8Detector:
             return results
 
 
+# TODO: test this class
 class YOLOv5Detector:
     def __init__(self, model_name="yolov5s"):
         # Load YOLOv5 model
@@ -48,7 +50,7 @@ class MinimalPublisher(Node):
 
         self.topic_name_information = f"information_{camera_id}"
         self.publisher_information = self.create_publisher(
-            String, self.topic_name_information, 10
+            ClassCoordinates, self.topic_name_information, 10
         )
 
         self.subscription = self.create_subscription(
@@ -57,51 +59,18 @@ class MinimalPublisher(Node):
         self._cv_bridge = CvBridge()
         self.detector = YOLOv8Detector()
 
-    def toData(self, result, keypoint=False):
-        """Convert the object to JSON format."""
-        if result.probs is not None:
-            LOGGER.warning("Warning: Classify task do not support tojson yet.")
-            return
-
-        # Create list of detection dictionaries
-        resultat = []
-        data = result.boxes.data.cpu().tolist()
-        h, w = (1, 1)
-        for i, row in enumerate(data):  # xyxy, track_id if tracking, conf, class_id
-            # Calculate center x-coordinate of the bounding box
-            center_x = (row[0] + row[2]) / (2 * w)
-            # Map the center x-coordinate to the angle range [-95°, 95°]
-            angle = (center_x - 0.5) * 190 - 95
-
-            box = {
-                "x1": row[0] / w,
-                "y1": row[1] / h,
-                "x2": row[2] / w,
-                "y2": row[3] / h,
-                "angle": angle,
-            }
-            class_id = int(row[-1])
-            name = result.names[class_id]
-            results = {"name": name, "box": box}
-            if result.boxes.is_track:
-                results["track_id"] = int(row[-3])  # track ID
-            if result.masks:
-                # numpy array
-                x, y = result.masks.xy[i][:, 0], result.masks.xy[i][:, 1]
-                results["segments"] = {"x": (x / w).tolist(), "y": (y / h).tolist()}
-            if keypoint:
-                x, y, visible = (
-                    result.keypoints[i].data[0].cpu().unbind(dim=1)
-                )  # torch Tensor
-                results["keypoints"] = {
-                    "x": (x / w).tolist(),
-                    "y": (y / h).tolist(),
-                    "visible": visible.tolist(),
-                }
-            resultat.append(results)
-
-        # Convert detections to JSON
-        return resultat
+    def toData(self, result, classes):
+        x_box = (result[0] + result[2]) / 2
+        angle = (x_box - 0.5 * 1344) * (190 / 1344)
+        if self.camera_id == 4:
+            angle = angle - 90
+        elif self.camera_id == 3:
+            angle = angle + 90
+        elif self.camera_id == 2:
+            angle = angle + 180
+        class_id = int(result[-1])  # Get class ID
+        class_name = classes[class_id]  # Get class name
+        return angle % 360, class_name
 
     def listener_callback(self, image):
         self.get_logger().info(f"Image received from Camera {self.camera_id}")
@@ -118,16 +87,16 @@ class MinimalPublisher(Node):
                 annotated_image, "rgb8"
             )
             self.publisher_annotated_image.publish(encoded_annotated_image)
-
-            # Informations part
-            informations = String()
-            informations.data = str(
-                self.toData(results)
-            )  # TODO: Vérifier ici l'intégrité des données
-            self.publisher_information.publish(informations)
-            """self.publisher_information.publish(
-                String(data=str(self.toData(results)))
-            )"""
+            new_results = results[0].boxes.data.cpu().tolist()
+            class_names = results[0].names  # Assuming this is how you get class names
+            for result in new_results:
+                # Informations part
+                informations = ClassCoordinates()
+                informations.header.stamp = self.get_clock().now().to_msg()
+                angle, class_name = self.toData(result, class_names)
+                informations.classification = class_name
+                informations.angle = angle
+                self.publisher_information.publish(informations)
 
 
 def main(args=None):
