1d7f08c13d34118c54c6a17f1d1687a2d6c5b609
Wyzyx <nel.mehdi.elt@gmail.com>
2024-01-30 09:59:05 +0100

    Refactor loc_error_node.cpp to use a unified Data struct and calculate location error

--- rob_ws/src/cario_tools/src/loc_error_node.cpp
+++ rob_ws/src/cario_tools/src/loc_error_node.cpp
@@ -11,18 +11,15 @@
 using namespace tf;
 using namespace message_filters;
 
-struct GazeboData {
-    std::string model_name;
+const std::string tf_name = "base_footprint";
+struct Data {;
     double x;
     double y;
+    double theta;
 };
 
-struct TFData {
-    std::string frame_id;
-};
-
-std::queue<GazeboData> gazebo_fifo;
-std::queue<TFData> tf_fifo;
+std::queue<Data> gazebo_fifo;
+std::queue<Data> tf_fifo;
 
 
 /*
@@ -51,13 +48,51 @@ int main(int argc, char** argv)
 
 void callback1(const gazebo_msgs::ModelStates::ConstPtr& msg)
 {
-    ROS_INFO("Received message from topics: %s", msg->name.at(0).c_str());
+    if (!msg->name.empty()) {
+        Data data;
+        int id = 0;
+
+        for (int i = 0; i < msg->name.size(); i++) {
+            if (msg->name.at(i) == tf_name) {
+                id = i;
+                break;
+            }
+        }
+        data.x = msg->pose.at(id).position.x;
+        data.y = msg->pose.at(id).position.y;
+        data.theta = tf::getYaw(msg->pose.at(id).orientation);
+
+        gazebo_fifo.push(data);
+    }
     
 }
 
 void callback2(const tf::tfMessage::ConstPtr& msg)
 {
-    ROS_INFO("Received message from topics: %s", msg->transforms[0].child_frame_id.c_str());
+    Data data;
+    int id = 0;
+
+    for (int i = 0; i < msg->transforms.size(); i++) {
+        if (msg->transforms.at(i).child_frame_id == tf_name) {
+            id = i;
+            break;
+        }
+    }
+    data.x = msg->transforms.at(id).transform.translation.x;
+    data.y = msg->transforms.at(id).transform.translation.y;
+    data.theta = tf::getYaw(msg->transforms.at(id).transform.rotation);
+
+    tf_fifo.push(data);
+}
+
+Data get_loc_error(Data gazebo_data, Data tf_data)
+{
+    Data error;
+    error.x = gazebo_data.x - tf_data.x;
+    error.y = gazebo_data.y - tf_data.y;
+    error.theta = gazebo_data.theta - tf_data.theta;
+
+    return error;
 }
 
 int main(int argc, char** argv)
@@ -69,7 +104,18 @@ int main(int argc, char** argv)
     ros::Subscriber ground_truth_sub = nh.subscribe("/gazebo_msgs/model_states", 1, callback1);
     ros::Subscriber slam_loc_sub = nh.subscribe("/tf", 1, callback2);
 
+
     ros::spin();
+    while (1) {
+        if (!gazebo_fifo.empty() && !tf_fifo.empty()) {
+            Data gazebo_data = gazebo_fifo.front();
+            Data tf_data = tf_fifo.front();
+            Data error = get_loc_error(gazebo_data, tf_data);
+            ROS_INFO("x: %f, y: %f, theta: %f", error.x, error.y, error.theta);
+            gazebo_fifo.pop();
+            tf_fifo.pop();
+        }
+    }
 
     return 0;
 }
