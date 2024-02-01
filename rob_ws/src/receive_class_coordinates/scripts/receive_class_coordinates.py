9dec6063b13a6ccb8d38a86a00098e30e5bc8d8b
AntoineLevanen <antoinelevanen@gmail.com>
2024-02-01 17:02:27 +0100

    fixe receive_class_coordinates.py, add test node

--- rob_ws/src/receive_class_coordinates/scripts/receive_class_coordinates.py
+++ rob_ws/src/receive_class_coordinates/scripts/receive_class_coordinates.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 import rospy
 
+import os
+import sys
+sys.path.remove(os.path.dirname(__file__))
+
 from receive_class_coordinates.msg import ClassCoordinates
 
+
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.angle) # cast to c
+    rospy.loginfo(rospy.get_caller_id() + "angle: %s", data.angle) # cast to c
 
 
 def listener():
 
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("/information_1", String, callback)
+    rospy.Subscriber("/information_1", ClassCoordinates, callback)
     
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
