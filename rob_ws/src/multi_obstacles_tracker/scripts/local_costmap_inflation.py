4dfcfd14d0f6a61839c048a2cdb6aaf51689632b
ConstantRoux <constant.roux31@gmail.com>
2024-02-08 00:01:39 +0100

    Add local costmap inflater

--- rob_ws/src/multi_obstacles_tracker/scripts/local_costmap_inflation.py
+++ rob_ws/src/multi_obstacles_tracker/scripts/local_costmap_inflation.py
@@ -1,41 +1,67 @@
 #!/usr/bin/env python
 import rospy
+import threading
 import numpy as np
 import message_filters
-from nav_msgs.msg import OccupancyGrid
+import tf
+import copy
+from nav_msgs.msg import OccupancyGrid, Odometry
 from multi_obstacles_tracker_msgs.msg import CameraDetectionStampedArray
+from scipy.ndimage import gaussian_filter
 
-    
+mutex = threading.Lock()
+
 class LocalCostMapInflation:
-    def __init__(self) -> None:       
-        # subscribe to the topic /move_base/local_costmap/to_inflate_costmap to get raw points cloud
-        self.costmap_sub_ = message_filters.Subscriber("/to_inflate_costmap", OccupancyGrid)
+    def __init__(self) -> None: 
+        # subscribe to the topic /move_base/local_costmap/to_inflate_costmap
+        self.costmap_sub_ = rospy.Subscriber("/to_inflate_costmap", OccupancyGrid, self.costmapCallback)
 
-        # subscribe to the topic /information_1 to get raw points cloud
+        # subscribe to the topic /information_1
         self.camera_detection_sub_ = message_filters.Subscriber("/information_1", CameraDetectionStampedArray)
+
+        # subscribe to the topic /mobile_base_controller/odom
+        self.odom_sub_ = message_filters.Subscriber("/mobile_base_controller/odom", Odometry)
+
+        # synch the tow last subs
+        ts = message_filters.ApproximateTimeSynchronizer([self.camera_detection_sub_, self.odom_sub_], 1, 1)
+        ts.registerCallback(self.inflateCallback)
 
         # Publish to the topic /measures
         self.costmap_pub_ = rospy.Publisher("/move_base/local_costmap/costmap", OccupancyGrid, queue_size=1)
 
-        # create synchroniser
-        ts = message_filters.TimeSynchronizer([self.costmap_sub_, self.camera_detection_sub_], 10)
-        ts.registerCallback(self.inflateCallback) 
+        # the current costmap
+        self.costmap = None
+
+        # inflation dist
+        self.inflation_dist = 0.5
         
         # spin it
         rospy.spin()
 
-    def inflateCallback(self, costmap: OccupancyGrid, detections: CameraDetectionStampedArray):
+    def costmapCallback(self, costmap: OccupancyGrid):
+        with mutex:
+            self.costmap = costmap
+
+    def inflateCallback(self, detections: CameraDetectionStampedArray, odom: Odometry):
         # copy the current map
-        inflated_costmap = costmap
+        with mutex:
+            inflated_costmap = copy.copy(self.costmap)
+        if self.costmap is None:
+            return
         inflated_costmap_data_np = np.array(list(inflated_costmap.data))
 
         # init the global costmap mask
-        global_costmap_mask = inflated_costmap
+        global_costmap_mask = copy.copy(inflated_costmap)
         global_costmap_data_np = np.array(list(inflated_costmap.data)) * 0
 
         # get metadata of the map
-        width, height = inflated_costmap.info.width, inflated_costmap.info.height
+        width_max, height_max = inflated_costmap.info.width, inflated_costmap.info.height
         resolution = inflated_costmap.info.resolution
+
+        # get rotation angle between odom and map
+        quaternion = (odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w)
+        euler_angles = tf.transformations.euler_from_quaternion(quaternion)
+        z_tf = euler_angles[2]
 
         for detection in detections.detections:
             # get covariance matrix components
@@ -44,8 +70,8 @@ class LocalCostMapInflation:
             c = detection.covariance[2]
             
             # get minor major axes and theta
-            lambda_1 = 2*np.sqrt(9.21*np.abs((a+c)/2 + np.sqrt(((a-c)**2)/4 + b**2)))
-            lambda_2 = 2*np.sqrt(9.21*np.abs((a+c)/2 - np.sqrt(((a-c)**2)/4 + b**2)))
+            lambda_1 = np.abs((a+c)/2 + np.sqrt(((a-c)**2)/4 + b**2))
+            lambda_2 = np.abs((a+c)/2 - np.sqrt(((a-c)**2)/4 + b**2))
             theta = 0
             if b == 0 and a >= c:
                 theta = 0
@@ -53,61 +79,76 @@ class LocalCostMapInflation:
                 theta = np.pi/2
             else:
                 theta = np.arctan2(lambda_1-a, b)
-
-            print(theta - detection.coordinates[1])
-
+            
             # get center of the ellipse in meter
-            ellipse_center = detection.coordinates[0] * np.array([np.cos(detection.coordinates[1]), np.sin(detection.coordinates[1])])
+            ellipse_center = detection.coordinates[0] * np.array([np.cos(detection.coordinates[1]*np.pi/180.0), np.sin(detection.coordinates[1]*np.pi/180.0)])
 
             # exclude elements out of the local costmap
-            if (np.abs(ellipse_center[0]) > width * resolution) or (np.abs(ellipse_center[1]) > height * resolution):
+            if (np.abs(ellipse_center[0]) > (width_max * resolution)) or (np.abs(ellipse_center[1]) > (height_max * resolution)):
                 # skip the current detection
                 continue
 
-            # get pos of bounding box top left corner of the ellipse in meter
-            OA = 1.96 * np.sqrt(lambda_1) * np.array([np.cos(theta), np.sin(theta)])
-            OB = 1.96 * np.sqrt(lambda_2) * np.array([np.sin(theta), -np.cos(theta)])
-            top_left_corner = OA - OB + ellipse_center
-
             # converts meter to pixels
-            ellipse_center_px = ellipse_center / resolution
-            top_left_corner_px = top_left_corner / resolution
-            width_px = (2 * lambda_2) / resolution
-            height_px = (2 * lambda_1) / resolution
+            ellipse_center_px = (ellipse_center / resolution).astype(int)
+            width_px = ((2 * lambda_2) / resolution).astype(int)
+            height_px = ((2 * lambda_1) / resolution).astype(int)
 
             # create an empty mask of the costmap
-            costmap_mask = inflated_costmap
-            costmap_mask_data_np = np.array(list(inflated_costmap.data)) * 0
+            costmap_mask = copy.copy(inflated_costmap)
+            costmap_mask_data_np = np.array(list(costmap_mask.data)) * 0
         
             # browse through each pixel of the bounding box in the costmap mask
             for x in range(-width_px//2, width_px//2):
-                for y in range(-height_px//2, height_px//2):
+                for y in range(-int(height_px//2), int(height_px//2)):
+                    # compute tf matrix from ellipse frame to robot frame
+                    T_r_e = np.array([[np.cos(theta), -np.sin(theta), ellipse_center_px[0]], 
+                                      [np.sin(theta), np.cos(theta), ellipse_center_px[1]],
+                                      [0, 0, 1]])
+                    
+                    T_m_r = np.array([[np.cos(z_tf), -np.sin(z_tf), width_max/2],
+                                      [np.sin(z_tf), np.cos(z_tf), height_max/2],
+                                      [0, 0, 1]])
+
                     # compute pixel position of the bounding box
-                    x_px = int(x * np.cos(theta) + width_px//2 + top_left_corner[0]) 
-                    y_px = int(y * np.sin(theta) + height_px//2 + top_left_corner[1])
+                    X_px = T_m_r @ T_r_e @ np.array([x, y, 1]).T
+                    x_px = int(X_px[0])
+                    y_px = int(X_px[1])
 
                     # check if the pixel is in the costmap
-                    if x_px > width or y_px > height:
+                    if x_px >= width_max or y_px >= height_max:
+                        # skip this pixel
+                        continue
+
+                    if x_px < 0 or y_px < 0:
                         # skip this pixel
                         continue
                     
                     # write the binary pixel to the mask
-                    costmap_mask_data_np[x_px + y_px * height] = 1
+                    costmap_mask_data_np[x_px + y_px * height_max] = 1
             
             # update data of the global costmap mask
             global_costmap_data_np |= costmap_mask_data_np
 
-            print(global_costmap_data_np)
+        # scale global costmap data
+        global_costmap_data_np *= 100
         
-        # # reshape global costmap data
-        # reshaped_global_costmap_data_np = np.reshape(global_costmap_data_np, (width, height)) 
+        # reshape global costmap data
+        reshaped_global_costmap_data_np = np.reshape(global_costmap_data_np, (width_max, height_max)) 
 
-        # # scale global costmap data
-        # reshaped_global_costmap_data_np *= 255
+        # apply gaussian filter to the global costmap
+        inflation_pixel = int(self.inflation_dist / resolution)
+        filtered_reshaped_global_costmap_data_np = gaussian_filter(reshaped_global_costmap_data_np, sigma=2, radius=inflation_pixel)
 
-        # apply global costmap mask to the inflated costmap
-        inflated_costmap = np.max(inflated_costmap_data_np, global_costmap_data_np)
+        # unshape the filtered global costmap
+        filtered_global_costmap_data_np = np.reshape(filtered_reshaped_global_costmap_data_np, (width_max * height_max,))
 
+        # get the max between ros local costmap and human detections costmap
+        global_costmap_data_np = np.maximum(filtered_global_costmap_data_np, inflated_costmap_data_np)
+        
+        # fill inflated costmap data
+        inflated_costmap.data = tuple((global_costmap_data_np).tolist())
+
+        # publish it!
         self.costmap_pub_.publish(inflated_costmap)
 
         
--- rob_ws/src/multi_obstacles_tracker/scripts/test_CameraDetectionsStamped.py
+++ rob_ws/src/multi_obstacles_tracker/scripts/test_CameraDetectionsStamped.py
@@ -24,11 +24,19 @@ def talker():
         my_msg_array.header = my_msg.header
 
         my_msg.classification = "person"
-        my_msg.coordinates[0] = 0.0
-        my_msg.covariance = [1.0, 0.0, 0.3, 0.0]
-        my_msg.coordinates[1] = 1
- 
+        my_msg.coordinates[0] = 1.40
+        my_msg.coordinates[1] = 160.5
+        my_msg.covariance = [0.37, 0.61, 0.61, 1.87]
+        
         my_msg_array.detections.append(my_msg)
+
+        my_msg2 = CameraDetectionStamped()
+        my_msg2.classification = "person"
+        my_msg2.coordinates[0] = 0.58
+        my_msg2.coordinates[1] = 290.42
+        my_msg2.covariance = [0.20, 0.06, 0.606, 0.05]
+        
+        my_msg_array.detections.append(my_msg2)
 
         pub.publish(my_msg_array)
 
