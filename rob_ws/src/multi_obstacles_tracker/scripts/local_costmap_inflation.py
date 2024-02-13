dd9103cc13b57b9c6e5efcef1f5f654bebfb782c
PGE-2023 <83353387+Dams66@users.noreply.github.com>
2024-02-13 11:07:46 +0100

    Change inflation error confidence

--- rob_ws/src/multi_obstacles_tracker/scripts/local_costmap_inflation.py
+++ rob_ws/src/multi_obstacles_tracker/scripts/local_costmap_inflation.py
@@ -110,8 +110,8 @@ class LocalCostMapInflation:
             # c = 0
 
             # get minor major axes and theta
-            lambda_1 = 1.5 * np.abs((a + c) / 2 + np.sqrt(((a - c) ** 2) / 4 + b**2))
-            lambda_2 = 1.5 * np.abs((a + c) / 2 - np.sqrt(((a - c) ** 2) / 4 + b**2))
+            lambda_1 = np.abs((a + c) / 2 + np.sqrt(((a - c) ** 2) / 4 + b**2))
+            lambda_2 = np.abs((a + c) / 2 - np.sqrt(((a - c) ** 2) / 4 + b**2))
             theta = 0
             if b == 0 and a >= c:
                 theta = 0
@@ -119,6 +119,8 @@ class LocalCostMapInflation:
                 theta = np.pi / 2
             else:
                 theta = np.arctan2(lambda_1 - a, b)
+
+            theta = -theta
 
             # get center of the ellipse in meter
             ellipse_center = detection.coordinates[0] * np.array(
